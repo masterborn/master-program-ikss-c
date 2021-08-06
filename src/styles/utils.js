bd028e3892d37895942a66e28b031f48b9967590
Mateusz Witkowski <mateusz.w@masterborn.com>
2021-08-06 14:31:17 +0200

    fix: remove unused variables

--- src/styles/utils.js
+++ src/styles/utils.js
@@ -1,13 +1,5 @@
-import { DefaultTheme } from 'styled-components';
-import { color, fontFamily, fontWeight } from './theme';
+export const getColor = (colorName) => (theme) => theme.color[colorName];
 
+export const getFontFamily = (fontName) => (theme) => theme.fontFamily[fontName];
 
-export const getColor = (colorName) => (
-  theme
- => theme.color[colorName])
-
-export const getFontFamily = (fontName) => (
-  theme => theme.fontFamily[fontName])
-
-export const getFontWeight = (fontWeightName) => (
-  theme  => theme.fontWeight[fontWeightName])
+export const getFontWeight = (fontWeightName) => (theme) => theme.fontWeight[fontWeightName];
