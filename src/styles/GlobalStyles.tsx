d55430f396e89d02a6bd0d54cdd2d474d6e8ca0e
Mariusz Olszewski <ssssspam11@gmail.com>
2021-08-11 23:06:14 +0200

    Add typography

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -6,7 +6,61 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
+  };
+  body {
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 32px;
+    letter-spacing: -0.015em;
+    color: #61798B;
+  };
+  h1 {
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 56px;
+    line-height: 72px;
+    letter-spacing: -0.015em;
+    color: #1A2847;
+  };
+  h2 {
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 40px;
+    line-height: 56px;
+    letter-spacing: -0.015em;
+    color: #1A2847;    
+  };
+  h3 {
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 44px;
+    letter-spacing: -0.015em;
+    color: #1A2847;
+  };
+  h4 {
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: -0.015em;
+    color: #1A2847;
+  };
+  h5{
+    font-family: Mulish;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: -0.015em;
+    color: #1A2847;    
+  };
 `;
 
 export default GlobalStyles;
