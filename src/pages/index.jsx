f5f1ee347d64f480d26a6ddc0373f6253fc20696
Mateusz Zajac <matzaj101@gmail.com>
2021-08-17 20:54:13 +0200

    add prettier

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,39 +1,45 @@
+import axios from 'axios';
+import MainPageProjectList from '@root/components/main/projects/MainPageProjectList';
 
-import axios from "axios";
-import MainPageProjectList from "@root/components/main/projects/MainPageProjectList";
-
-
-function HomePage(props){
-    const {projects, boardMembers, basicContent, partnerLogo}=props;
+function HomePage(props) {
+  const { projects, boardMembers, basicContent, partnerLogo } = props;
   return (
     <>
-        <MainPageProjectList projects={projects}/>
+      <MainPageProjectList projects={projects} />
     </>
-  )
-};
+  );
+}
 
 export default HomePage;
 
-
 export async function getStaticProps() {
-    let allEntries;
-    try {        
-        allEntries = await axios.get(`https://cdn.contentful.com/spaces/${process.env.SPACE}/environments/master/entries?access_token=${process.env.TOKEN}`);
-    }
-    catch (error) {
-        throw Error('something went wrong! ')
-    }
-    const projects = allEntries.data.items.filter(item => item.sys.contentType.sys.id === 'projects');
-    const boardMembers = allEntries.data.items.filter(item => item.sys.contentType.sys.id === 'boardMembers');
-    const basicContent = allEntries.data.items.filter(item => item.sys.contentType.sys.id === 'basicContent');
-    const partnerLogo = allEntries.data.items.filter(item => item.sys.contentType.sys.id === 'partnerLogos');
-    const showOnMainPage = projects.filter(p=>p.fields.showOnHomepage);
-    return {
-        props: {
-            projects: showOnMainPage,
-            boardMembers,
-            basicContent,
-            partnerLogo
-        }
-    }
+  let allEntries;
+  try {
+    allEntries = await axios.get(
+      `https://cdn.contentful.com/spaces/${process.env.SPACE}/environments/master/entries?access_token=${process.env.TOKEN}`,
+    );
+  } catch (error) {
+    throw Error('something went wrong! ');
+  }
+  const projects = allEntries.data.items.filter(
+    (item) => item.sys.contentType.sys.id === 'projects',
+  );
+  const boardMembers = allEntries.data.items.filter(
+    (item) => item.sys.contentType.sys.id === 'boardMembers',
+  );
+  const basicContent = allEntries.data.items.filter(
+    (item) => item.sys.contentType.sys.id === 'basicContent',
+  );
+  const partnerLogo = allEntries.data.items.filter(
+    (item) => item.sys.contentType.sys.id === 'partnerLogos',
+  );
+  const showOnMainPage = projects.filter((p) => p.fields.showOnHomepage);
+  return {
+    props: {
+      projects: showOnMainPage,
+      boardMembers,
+      basicContent,
+      partnerLogo,
+    },
+  };
 }
