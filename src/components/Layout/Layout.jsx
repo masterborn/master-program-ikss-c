ae426e0f772c8f7005badfa9eb7d8ee84f1e52e3
WojtekObl <wojtek.obloza@gmail.com>
2021-08-26 12:30:03 +0200

    layout changes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,13 +4,13 @@ import Navbar from './Navbar/Navbar';
 function Layout({ children, props }) {
   const socialLinks = props.content?.filter((position) => position.fields.linkUrl !== undefined);
   const footerText = props.content.find((position) => position.fields.identifier === 'footer-text');
-  const pageName= {props}
+  const pageName = { props };
 
   return (
     <>
       <Navbar socialLinks={socialLinks} />
       <main>{children}</main>
-      <Footer socialLinks={socialLinks} footerText={footerText} pageName={pageName}/>
+      <Footer socialLinks={socialLinks} footerText={footerText} pageName={pageName} />
     </>
   );
 }
