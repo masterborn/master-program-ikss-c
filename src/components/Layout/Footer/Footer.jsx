c216c7b97b446dc8dcd837d70ef19d797123b5d9
WojtekObl <wojtek.obloza@gmail.com>
2021-08-26 12:15:02 +0200

    problem with logo svg fixed

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -2,33 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import { color } from '@root/styles/GlobalStyles';
 import LogoIkss from '@root/components/logoIkss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-
-function Footer({ socialLinks, footerText, pageName}) {
-
+function Footer({ socialLinks, footerText, pageName }) {
   const StyledFooter = styled.footer`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: space-around;
     background-color: ${color.blue};
-    height: ${pageName === 'HomePage' ? '728px' : '404px'};
     width: 100%;
-    & p {
-      color: ${color.white}
+    & a,
+    p {
+      color: ${color.white};
+      text-align: center;
+    }
+    & ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      width: 300px;
+      justify-content: space-between;
     }
   `;
 
   return (
-    <StyledFooter>
+    <StyledFooter style={{ height: `${pageName === 'HomePage' ? '728px' : '404px'}` }}>
       <ul>
         {socialLinks.map((link) => (
           <li key={link.fields.identifier}>
-            <a href={link.fields.linkUrl}>{link.fields.identifier}</a>
+            <a href={link.fields.linkUrl}>{link.fields.linkCaption}</a>
           </li>
         ))}
       </ul>
       <LogoIkss />
-      <p>{footerText?.fields.title}</p>
+      <p>
+        {footerText?.fields.title}
+        <p>
+          Made with <FontAwesomeIcon icon={faHeart} /> by{' '}
+          <a href="https://masterborn.com/" target="blank">
+            MasterBorn Software
+          </a>
+        </p>
+      </p>
     </StyledFooter>
   );
 }
