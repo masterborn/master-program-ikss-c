23b609de37de84a892fd4233f363b4937640cd1e
Mateusz Zajac <matzaj101@gmail.com>
2021-08-26 11:00:48 +0200

    add error page

--- src/api/apiClient/index.js
+++ src/api/apiClient/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '../../../config';
 
-const entryUrl = `https://cdn.contentful.com/spaces/${config.CONTENTFUL_API_SPACE}/environments/master/entries?access_toke=${config.CONTENTFUL_API_TOKEN}`;
+const entryUrl = `https://cdn.contentful.com/spaces/${config.CONTENTFUL_API_SPACE}/environments/master/entries?access_token=${config.CONTENTFUL_API_TOKEN}`;
 const assetUrl = `https://cdn.contentful.com/spaces/${config.CONTENTFUL_API_SPACE}/environments/master/assets?access_token=${config.CONTENTFUL_API_TOKEN}`;
 
 class ApiCLient {
@@ -14,8 +14,15 @@ class ApiCLient {
       try {
         return await axios.get(`${this.url}`);
       } catch (e) {
-        throw Error('something went wrong');
-        // return Promise.reject(e.message);
+        if (e.response.status === 404) {
+          throw Error('data not found');
+        } else if (e.response.status === 401) {
+          throw Error('you are not authorized');
+        } else if (e.response.status === 500) {
+          throw Error('sorry.. we are working on this right now');
+        } else {
+          throw Error('something went wrong');
+        }
       }
     };
   }
