6a93610897419d9940049b50dbaaedc1d63cf1cb
WojtekObl <wojtek.obloza@gmail.com>
2021-08-26 13:25:06 +0200

    checking for deploy isues

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,18 @@
-import Footer from './Footer/Footer';
+// import Footer from './Footer/Footer';
 import Navbar from './Navbar/Navbar';
 
 function Layout({ children, props }) {
-  const socialLinks = props.content?.filter((position) => position.fields.linkUrl !== undefined);
-  const footerText = props.content?.find(
-    (position) => position.fields.identifier === 'footer-text',
-  );
-  const pageName = { props };
+  // const socialLinks = props.content?.filter((position) => position.fields.linkUrl !== undefined);
+  // const footerText = props.content?.find(
+  //   (position) => position.fields.identifier === 'footer-text',
+  // );
+  // const pageName = { props };
 
   return (
     <>
       <Navbar socialLinks={socialLinks} />
       <main>{children}</main>
-      <Footer socialLinks={socialLinks} footerText={footerText} pageName={pageName} />
+      {/* <Footer socialLinks={socialLinks} footerText={footerText} pageName={pageName} /> */}
     </>
   );
 }
