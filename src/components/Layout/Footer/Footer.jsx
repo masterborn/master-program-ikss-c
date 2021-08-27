b7e0ed72480f834cffa07e0ae0e46b95e7ac5ef8
WojtekObl <wojtek.obloza@gmail.com>
2021-08-27 11:18:52 +0200

    new logo for footer, file fixed

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color } from '@root/styles/GlobalStyles';
 import LogoIkss from '@root/components/logoIkss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { color } from '@root/styles/GlobalStyles';
 
 function Footer({ socialLinks, footerText, pageName }) {
   const StyledFooter = styled.footer`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-around;
+    justify-content: flex-end;
     background-color: ${color.blue};
     width: 100%;
+    padding: 0 0 56px 0;
     & a,
     p {
       color: ${color.white};
       text-align: center;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 28px;
+      letter-spacing: -0.015em;
     }
     & ul {
       list-style-type: none;
@@ -42,9 +48,7 @@ function Footer({ socialLinks, footerText, pageName }) {
         {footerText?.fields.title}
         <p>
           Made with <FontAwesomeIcon icon={faHeart} /> by{' '}
-          <a href="https://masterborn.com/" target="blank">
-            MasterBorn Software
-          </a>
+          <a href="https://masterborn.com/">MasterBorn Software</a>
         </p>
       </p>
     </StyledFooter>
