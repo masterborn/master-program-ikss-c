db752aefd0da19255fd1faa756b5011106d0f443
WojtekObl <wojtek.obloza@gmail.com>
2021-08-30 17:05:18 +0200

    scroll up fn added

--- src/components/UI/Icons/ScrollUpButton.jsx
+++ src/components/UI/Icons/ScrollUpButton.jsx
@@ -1,9 +1,23 @@
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState, useEffect } from 'react';
 
 export default function ScrollUpButton() {
+  const [browserWindow, setBrowserWindow] = useState({});
+
+  useEffect(() => {
+    setBrowserWindow(window);
+  }, []);
+
+  const scrollToTop = () => {
+    browserWindow.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <button type="button" className="ScrollUpButtonBody">
+    <button type="button" onClick={scrollToTop} className="ScrollUpButtonBody">
       <FontAwesomeIcon icon={faChevronUp} />
     </button>
   );
