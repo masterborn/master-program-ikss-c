e3e0792733eb391c1264d680e062ab3a5e7096a6
WojtekObl <wojtek.obloza@gmail.com>
2021-08-31 13:09:40 +0200

    show socials btns after scroll addded

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PrimaryButton } from '@root/components/UI/Button/Button';
 import styled from 'styled-components';
 import LogoIkssFrame from '@root/components/logoIkssFrame';
@@ -15,6 +15,7 @@ const StyledNav = styled.nav`
   box-shadow: 0px 4px 16px rgba(97, 121, 139, 0.1);
   position: fixed;
   top: 0;
+  z-index: 100;
   & .wrapper {
     display: flex;
     height: 100%;
@@ -28,12 +29,30 @@ const StyledNav = styled.nav`
 `;
 
 function Navbar({ socialLinks, pathname }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showAfterScroll = () => {
+    const heightToShowFrom = 600;
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToShowFrom) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', showAfterScroll);
+    return () => window.removeEventListener('scroll', showAfterScroll);
+  }, []);
+
   return (
     <StyledNav>
       <div className="wrapper">
         <LogoIkssFrame />
         <NavigationMenu pathname={pathname} />
-        <SocialButtons socialLinks={socialLinks} size="12px" body="24px" />
+        {isVisible && <SocialButtons socialLinks={socialLinks} size="12px" body="24px" />}
         <PrimaryButton size="small">Skontaktuj się</PrimaryButton>
       </div>
     </StyledNav>
