bb17a2fae7c263d615574c3421b04ab2fa2e61f0
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-15 20:36:34 +0200

    End task

---
+++ src/pages/wspolpraca/index.jsx
@@ -0,0 +1,29 @@
+import getAllAssets from '@root/api/assetClient';
+import Section from '@root/components/UI/Section/section';
+import { getBasicContent } from '@root/api/cmsClient';
+
+function Cooperation(props) {
+  const { assets, content } = props;
+  const topSection = content.filter((x) => x.fields.identifier === 'cooperation-top-section');
+  const bottomCta = content.filter((x) => x.fields.identifier === 'cooperation-bottom-cta');
+
+  return (
+    <>
+      <Section short content={topSection} assets={assets} />
+      <Section content={bottomCta} />
+    </>
+  );
+}
+export default Cooperation;
+
+export async function getStaticProps() {
+  const content = await getBasicContent();
+  const assets = await getAllAssets();
+
+  return {
+    props: {
+      content,
+      assets,
+    },
+  };
+}
