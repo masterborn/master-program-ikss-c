fa92c4a91eff65518664a3c08c1981cef7aaa61c
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-15 19:38:09 +0200

    Finish task

--- src/pages/o-nas/index.jsx
+++ src/pages/o-nas/index.jsx
@@ -1,14 +1,20 @@
 import getAllAssets from '@root/api/assetClient';
 import Mission from '@root/aboutUs/Mission/Mission';
 import History from '@root/aboutUs/History/History';
+import Section from '@root/components/UI/Section/section';
 import { getBasicContent } from '@root/api/cmsClient';
 
 function AboutUsPage(props) {
   const { assets, content } = props;
+  const topSection = content.filter((x) => x.fields.identifier === 'about-us-top-section');
+  const bottomCta = content.filter((x) => x.fields.identifier === 'about-us-bottom-cta');
+
   return (
     <>
+      <Section content={topSection} assets={assets} />
       <Mission content={content} assets={assets} />
       <History content={content} assets={assets} />
+      <Section content={bottomCta} />
     </>
   );
 }
