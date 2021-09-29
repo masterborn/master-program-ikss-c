7d0353a830d2911f6d32864c02d80b17e92d1bee
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-29 20:57:58 +0200

    Remove bad title

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -15,7 +15,6 @@ function Meta() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/keen-slider@latest/keen-slider.min.css"
       />
-      <title>Ikss</title>
     </Head>
   );
 }
