a5be5b09785f04e5df7770031cb11150c5e5e9ae
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-30 01:13:17 +0200

    Favicon

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -4,7 +4,7 @@ function Meta() {
   return (
     <Head>
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      <link rel="shortcut icon" href="/public/static/favicon.svg" />
+      <link rel="shortcut icon" href="/public/favicon.svg" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
