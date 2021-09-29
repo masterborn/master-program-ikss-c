869753c0c85e508545fb9780a1f70c8ddf47156e
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-29 20:09:27 +0200

    Add meta to pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import getAllAssets from '@root/api/assetClient';
+import PageHead from '@root/components/Head';
 import MainPagePartnersList from '@root/components/main/partners/MainPagePartnersList';
 import Banner from '@root/components/main/projects/Banner/banner';
 import MainPageProjectList from '@root/components/main/projects/MainPageProjectList';
@@ -8,12 +9,19 @@ import { getBasicContent, getPartnerLogos, getProjects } from '../api/cmsClient/
 
 function HomePage(props) {
   const { partners, assets, content, projects } = props;
+    const head = content.filter((x) => x.fields.identifier === 'homepage-meta');
   return (
     <>
+      <PageHead head={head} />
       <Banner content={content} asset={assets} />
       <HomepageValues content={content} assets={assets} />
       <MainPageProjectList projects={projects} assets={assets} />
-      <MainPagePartnersList logosDisplayedOnHomepage partners={partners} assets={assets} content={content} />
+      <MainPagePartnersList
+        logosDisplayedOnHomepage
+        partners={partners}
+        assets={assets}
+        content={content}
+      />
       <Form content={content} />
     </>
   );
--- src/pages/wspolpraca/index.jsx
+++ src/pages/wspolpraca/index.jsx
@@ -1,4 +1,5 @@
 import Section from '@root/components/UI/Section/section';
+import PageHead from '@root/components/Head';
 import MainPagePartnersList from '@root/components/main/partners/MainPagePartnersList';
 import Highlights from '@root/components/main/Values/highlights';
 import { getBasicContent, getPartnerLogos } from '@root/api/cmsClient';
@@ -8,8 +9,11 @@ function Cooperation(props) {
   const { assets, content, partners } = props;
   const topSection = content.filter((x) => x.fields.identifier === 'cooperation-top-section');
   const bottomCta = content.filter((x) => x.fields.identifier === 'cooperation-bottom-cta');
+  const head = content.filter((x) => x.fields.identifier === 'cooperation-meta');
+
   return (
     <>
+      <PageHead head={head} />
       <Section shortTopParagraph content={topSection} assets={assets} />
       <Highlights content={content} assets={assets} />
       <MainPagePartnersList partners={partners} assets={assets} content={content} />
