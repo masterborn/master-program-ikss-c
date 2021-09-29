917b7db6a8d4c19be7f69e2b696cfdcdbcc87473
Mariusz Olszewski <ssssspam11@gmail.com>
2021-09-30 01:05:04 +0200

    Import favicon

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
+import favicon from 'public/static/favicon.svg'
 
 function Meta() {
   return (
     <Head>
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      <link rel="shortcut icon" href="/public/static/favicon.svg" />
+      <link rel="shortcut icon" href={favicon} />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
